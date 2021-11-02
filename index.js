e9529d3226bd3236b4c96b24ca9cae3ed92354c5
Zach <zmommaerts@gmail.com>
2021-11-01 21:35:00 -0500

    commit tree 31e51850f98834cc339d04566971a8966ad3d1a1

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-const num1 = 31;
-const num2 = 2;
-const multiply = num1 * num2;
+function multiply (num1, num2) {
+    return num1 * num2;
+}
 
-const random = Math.floor(Math.random() * 100) + 1;
+// const num1 = 31;
+// const num2 = 2;
+// const multiply = num1 * num2;
 
-const num3 = 24;
-const num4 = 5;
-const mod = num3 % num4;
+// const random = Math.floor(Math.random() * 100) + 1;
 
-const max = Math.max(3, 5, 7, 15, 16, 19, 20);
+// const num3 = 24;
+// const num4 = 5;
+// const mod = num3 % num4;
+
+// const max = Math.max(3, 5, 7, 15, 16, 19, 20);
